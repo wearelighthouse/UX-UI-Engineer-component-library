c6c5b72b0f0d8668a0d8c1458228fab08be2aef3
Betsimisaraka <anita.ras@onja.org>
2022-06-30 15:25:22 +0300

    Styling the Modal content

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -15,11 +15,13 @@ const Modal = ({children, ...props}) => {
         <Button onClick={() => handleClick()} variant="primary" size="lg">Open Modal</Button>
         <div className={`modal ${isShow ? 'active' : ''}`}>
             <div className="modal__content">
-                {children}
-                <div>
-                    <Button onClick={() => handleClick()} variant="primary" disabled={false} size="lg">X</Button>
-                    <Button onClick={() => handleClick()} variant="secondary" disabled={false} size="lg">Cancel</Button>
-                    <Button onClick={() => handleClick()} variant="secondary" disabled={false} size="lg">OK</Button>
+                <div className="close__modal">
+                    <Button onClick={handleClick} variant="primary" disabled={false} size="lg">X</Button>
+                </div>
+                <p className="modal__p">{children}</p>
+                <div className="modal__buttons">
+                    <Button onClick={handleClick} variant="secondary" disabled={false} size="lg">Cancel</Button>
+                    <Button onClick={handleClick} variant="secondary" disabled={false} size="lg">OK</Button>
                 </div>
             </div>
         </div>
